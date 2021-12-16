7e8bc10966519ce04141abbf052ccff0f0aebc1a
Anmol Sahoo <anmol.sahoo25@gmail.com>
2021-12-15 21:43:39 -0500

    updated step1

--- step-1/index.mjs
+++ step-1/index.mjs
@@ -7,35 +7,35 @@ const log = (msg) => console.log(`[APP]   : ${msg}`);
 
 /* log messages from Reach */
 const logReach = (addrs) => {
-	const f = ([s,e]) => {
-		const s1 = addrs[e[0]];
-		const s2 = typeof(addrs[e[1]]) === 'undefined' ?
-			(typeof(e[1]) === 'undefined' ? "" : e[1]) : addrs[e[1]];
-		const s3 = typeof(e[2]) === 'undefined' ? "" : e[2];
-		console.log(`[REACH] : ${s}${s1} ${s2}${s3}`);
-	};
+  const f = ([s,e]) => {
+    const s1 = addrs[e[0]];
+    const s2 = typeof(addrs[e[1]]) === 'undefined' ?
+      (typeof(e[1]) === 'undefined' ? "" : e[1]) : addrs[e[1]];
+    const s3 = typeof(e[2]) === 'undefined' ? "" : e[2];
+    console.log(`[REACH] : ${s}${s1} ${s2}${s3}`);
+  };
 
-	return f;
+  return f;
 };
 
 (async () => {
-	log("Starting application");
+  log("Starting application");
 
-	const startingBalance = stdlib.parseCurrency(100);
+  const startingBalance = stdlib.parseCurrency(100);
 
-	var addrs = {};
+  var addrs = {};
 
-	const acc0 = await stdlib.newTestAccount(startingBalance);
+  const acc0 = await stdlib.newTestAccount(startingBalance);
 
-	addrs[acc0.getAddress()] = "acc0"
+  addrs[acc0.getAddress()] = "acc0"
 
-	log(`acc0 (${acc0.getAddress().slice(0,4)}...) ${await stdlib.balanceOf(acc0)} microALGO`);
+  log(`acc0 (${acc0.getAddress().slice(0,4)}...) ${await stdlib.balanceOf(acc0)} microALGO`);
 
-	const ctc0 = acc0.contract(backend);
+  const ctc0 = acc0.contract(backend);
 
-	await Promise.all([
-		backend.Deployer(ctc0, {
-			log: logReach(addrs)
-		})
-	]);
+  await Promise.all([
+    backend.Deployer(ctc0, {
+      log: logReach(addrs)
+    })
+  ]);
 })();
